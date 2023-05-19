76da96a6f86ec7ec83088f94eb92475d73f8c630
Ronald Yin <zidyin_jx@126.com>
2023-05-19 00:47:38 -0400

    8-birthday-components, somehow still not working

--- src/components/DatePickerInput/index.js
+++ src/components/DatePickerInput/index.js
@@ -1,12 +1,45 @@
+import { DatePicker } from 'antd-mobile';
 import style from './index.module.css';
+import moment from 'moment';
+import PropTypes from 'prop-types';
 import datepickerIcon from '../../assets/datepicker-icon.svg';
+import { useState } from 'react';
 
-export default () => (
-    <div className={style.birthdayInput}>
-        <div className={style.birthdayTitleItem}>birthday</div>
-        <div>
-            <span className={style.birthdayPlaceholder}>YYYY/MM/DD </span>
-            <img className = {style.datepickerIcon} src = {datepickerIcon} alt = "datepickerIcon"></img>
+
+const DatePickerInput = ({
+    value,
+    onChange}) => {
+    const [visible,setVisible] = useState(false);
+    const onClickDatePicker = () => {
+        setVisible(true);
+    };
+    return (<>
+        <DatePicker
+            title='时间选择'
+            visible={visible}
+            onClose={() => {
+            setVisible(false);
+            }}
+            max={now}
+            onConfirm={(val) => {
+             onChange(val)
+            }}
+        />
+    
+        <div className={style.birthdayInput} onClick = {onClickDatePicker}>
+            <div className={style.birthdayTitleItem}>birthday</div>
+            <div>
+                <span className={style.birthdayPlaceholder}>{value ? moment(value).format('YYYY/MM/DD'): 'y/m/d' }</span>
+                <img className = {style.datepickerIcon} src = {datepickerIcon} alt = "datepickerIcon"></img>
+            </div>
         </div>
-    </div>
-);
+    </>
+    ); 
+};
+
+DatePickerInput.propTypes = {
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+};
+
+export default DatePickerInput;
